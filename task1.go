185ba45e12e50663a8f1877189a251b8213fccaf
pennat <91137684+pennat@users.noreply.github.com>
2022-04-25 12:48:33 +0300

    Обновленное задание 1

--- task1.go
+++ task1.go
@@ -8,13 +8,13 @@ func main() {
 	fmt.Println("Введите порядковый номер: ")
 	fmt.Scanln(&num)
 
-	fmt.Println(fibnum(num))
+	fmt.Println("Число Фибоначчи: ", fibnum(num))
 }
 
 func fibnum(num int) int {
-	if num == 0 {
+	if num == 1 {
 		return 0
-	} else if num == 1 || num == 2 {
+	} else if num == 2 || num == 3 {
 		return 1
 	}
 	return fibnum(num-1) + fibnum(num-2)
