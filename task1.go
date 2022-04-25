4c1e0d0f99470f9e9a89fa6766423f88171a868c
pennat <91137684+pennat@users.noreply.github.com>
2022-04-25 14:08:17 +0300

    Обновленное задание 1

--- task1.go
+++ task1.go
@@ -14,7 +14,7 @@ func main() {
 func fibnum(num int) int {
 	if num == 1 {
 		return 0
-	} else if num == 2 || num == 3 {
+	} else if num == 2 {
 		return 1
 	}
 	return fibnum(num-1) + fibnum(num-2)
